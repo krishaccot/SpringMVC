080a8595bd1216818e81096892cb4ef9ae609efb
krishaccot <krishaccot@gmail.com>
2024-01-26 14:13:08 -0800

    warning fix

--- src/main/java/com/krish/conference/controller/RegistrationController.java
+++ src/main/java/com/krish/conference/controller/RegistrationController.java
@@ -1,7 +1,5 @@
 package com.krish.conference.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
